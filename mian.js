da508c7dc040aa7e343df333ff094e04b36fb8bc
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-02-19 16:52:35 +0800

    huh

--- mian.js
+++ mian.js
@@ -6,16 +6,15 @@ const closeEyes = "(# -_-)o__/";
 const sleep = ["(# -_-)o__/", "(# -_-)z__/", "(# -_-)zZ_/", "(# -_-)zzZ/"];
 
 let normalMode = true;
-let timesOut;
 
 function change(times, n) {
     if (times > 0) {
         if (n < 4) {
             text.innerHTML = sleep[n];
-            n ++;
+            n++;
         } else {
             n = 0;
-            times --;
+            times--;
         }
         console.log(times);
         setTimeout("change(" + times + ',' + n + ")", 500);
@@ -25,24 +24,35 @@ function change(times, n) {
     }
 }
 function sleepF() {
+    normalMode = false;
     let times = Math.floor(Math.random() * 5);
     change(times, 0);
-    console.log(99);
+}
+function happyF() {
+    normalMode = false;
+    text.innerHTML = happy;
+    setTimeout("text.innerHTML = normal", Math.floor(Math.random() * 6) * 1000);
 }
 function winkEyes() {
-    setTimeout(() => {
-        text.innerHTML = closeEyes;
-        setTimeout("text.innerHTML = normal", 3000);
-    }, 90);
-}
-function stop() {
-    clearTimeout(timesOut);
+    if (normalMode) {
+        setTimeout(() => {
+            text.innerHTML = normal;
+            setTimeout(() => {
+                text.innerHTML = closeEyes;
+                winkEyes();
+            }, 3000);
+        }, 300);
+    }
 }
 function normalF() {
-    if(normalMode) {
-        winkEyes();
-        normalF();
-    }
+    winkEyes();
+    setTimeout(() => {
+        if (Math.random() >= 0.5) {
+            sleepF();
+        } else {
+            happyF();
+        }
+    }, Math.floor(Math.random() * 60) * 1000);
 }
 
 normalF();
